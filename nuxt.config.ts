22557a7482f80ce0210122db2b717731ec17a77d
amadiev <beckmad@AMs-MacBook-Air.local>
2026-02-20 22:21:00 +0300

    delete nitro config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,16 +36,6 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2024-07-11',
 
-  nitro: {
-    prerender: {
-      routes: [
-        '/**'
-      ],
-      crawlLinks: true,
-      autoSubfolderIndex: false
-    }
-  },
-
   eslint: {
     config: {
       stylistic: {
